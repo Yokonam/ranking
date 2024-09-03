41ac3c6cef38cdd1cd8cfaf5400431b3221aafbb
yokonam <s.yokonami@gmail.com>
2024-09-03 19:29:34 +0900

    Refactor astro.config.mjs to update adapter for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import vercelStatic from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercelStatic(),
+  output: 'server',
+  adapter: vercel(),
 });
